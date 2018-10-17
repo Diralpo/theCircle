dd6762071ae5ebfbb980a2666744cbaa3b93e6e5
rrrral <295536022@qq.com>
2018-10-17 11:24:03 +0800

    a demo

--- Webpage/js/loadWeb.js
+++ Webpage/js/loadWeb.js
@@ -21,11 +21,11 @@ var load_nav = function(){
     var createNav = function(){
         var adiv = document.createElement('div');
         adiv.setAttribute('text-align', 'center');
-        adiv.innerHTML =    "<a href=\"index.html\" style=\"font-size:18px;\">主页</a> | "+
-                            "<a href=\"login.html\" style=\"font-size:18px;\">登录</a> | " +
-                            "<a href=\"signup.html\" style=\"font-size:18px;\">注册</a> | " +
-                            "<a href=\"query.html\" style=\"font-size:18px;\">查询</a> | " +
-                            "<a href=\"model.html\" style=\"font-size:18px;\">模板</a>";
+        adiv.innerHTML =    "<a href=\"..\\index.html\" style=\"font-size:18px;\">主页</a> | "+
+                            "<a href=\"..\\static\\login.html\" style=\"font-size:18px;\">登录</a> | " +
+                            "<a href=\"..\\static\\signup.html\" style=\"font-size:18px;\">注册</a> | " +
+                            "<a href=\"..\\static\\query.html\" style=\"font-size:18px;\">查询</a> | " +
+                            "<a href=\"..\\static\\model.html\" style=\"font-size:18px;\">模板</a>";
         return adiv
     };
     var theNav = document.getElementById("theNav");
