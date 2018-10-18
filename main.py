a972866d2e12249665e607ad30bc7e6a547df835
rrrral <295536022@qq.com>
2018-10-18 20:05:33 +0800

    no nginx

--- main.py
+++ main.py
@@ -4,6 +4,4 @@
 from service.server import *
 
 if __name__ == '__main__':
-    db, cursor = db_helper.login_db(const.DB_USER, const.DB_PASS, const.DB_NAME)
-    app.run(host=const.SERVER_IP, port=const.SERVER_PORT)
-
+    app.run(debug=True, port=80)
--- service/server.py
+++ service/server.py
@@ -1,109 +1,15 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import time
-
-from flask import Flask, json
-from flask import request
-from flask_cors import *
-import hashlib
-
-from common import db_helper
-from config import const
-from common import users_helper
-
-app = Flask(__name__, template_folder='../Webpage/')
-CORS(app, supports_credentials=True)
-db, cursor = db_helper.login_db(const.DB_USER, const.DB_PASS, const.DB_NAME)
+from flask_config import *
+from flask import render_template
 
 
-@app.route('/signup', methods=['GET', 'POST'])
-def get_the_ip():
-    if request.method == 'POST':
-        ip = request.remote_addr
-        user_data = request.get_json()
-        user_data['ip'] = str(ip)
-
-        tran_str = user_data['username']
-        hl = hashlib.md5()
-        hl.update(tran_str.encode(encoding='utf-8'))
-        verified_url = hl.hexdigest()
-        tran_str = str(time.time())
-        hl.update(tran_str.encode(encoding='utf-8'))
-        verified_url = verified_url + hl.hexdigest()
-        tran_str = user_data['email']
-        hl.update(tran_str.encode(encoding='utf-8'))
-        verified_url = verified_url + hl.hexdigest()
-
-        user_data['verified_url'] = verified_url
-        user_data['signup_time'] = time.time()
-        print('验证邮箱的url为： ' + verified_url)
-
-        users_helper.insert_user(db, cursor, user_data)
-        return '注册成功，请前往邮箱确认'
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-@app.route('/verification/<verified_url>/', methods=['GET'])
-def user_verify(verified_url):
-    #  邮箱验证
-    if users_helper.verify_user(db, cursor, verified_url) == 0:
-        return "注册成功"
-    else:
-        return "404 <br/> NOT FOUND"
-
-
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    data = [{'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'},
-            {'姓名': 'xx', '学号': 1234, '性别': 'male', '联系方式': '29xxx'}]
-    return json.dumps(data, encoding='utf-8')
-
-
-if __name__ == '__main__':
-    app.run(host=const.SERVER_IP, port=const.SERVER_PORT)
+@app.route('/user/<name>/')
+def show_user(name):
+    return render_template('user.html', name=name)
