57546a504133ddab1a5b4af9c653d926414f3863
46436 <464365439@qq.com>
2018-10-18 21:23:43 +0800

    initial commit 1

--- src/main/java/com/demo/config/DemoConfig.java
+++ src/main/java/com/demo/config/DemoConfig.java
@@ -8,6 +8,7 @@ import com.jfinal.template.Engine;
 public class DemoConfig extends JFinalConfig {
 
 
+    
     public static void main(String args[]) {
         JFinal.start("src/main/webapp", 80, "/", 2);
     }
