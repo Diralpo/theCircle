97a393d4c5f72baeb9d598e76684c173ff980067
rrrral <295536022@qq.com>
2018-10-18 21:07:17 +0800

    添加几个页面

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 
 from service.server import *
+from service.web_server import *
 
 if __name__ == '__main__':
     app.run(debug=True, port=80)
