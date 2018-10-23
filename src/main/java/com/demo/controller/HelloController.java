3c480a3fcce586c916606b47cbf69c80e1fc3203
rrrral <295536022@qq.com>
2018-10-23 23:04:41 +0800

    修复HelloController的bug，修改页脚

--- src/main/java/com/demo/controller/HelloController.java
+++ src/main/java/com/demo/controller/HelloController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 public class HelloController extends Controller {
 
     public void index() {
-        redirect("index.html");
+        redirect("/static/index.html");
     }
 
     public void login() {
