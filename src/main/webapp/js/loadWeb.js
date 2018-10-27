184b12fbf27a3bd948f398a178a3ecfbc2ec5494
rrrral <295536022@qq.com>
2018-10-27 20:04:35 +0800

    修复样式的一些问题

--- src/main/webapp/js/loadWeb.js
+++ src/main/webapp/js/loadWeb.js
@@ -1,34 +1,22 @@
-var startTime = function() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    // add a zero in front of numbers<10
-    h = checkTime(h);
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('theClock').innerHTML = h + ":" + m + ":" + s;
-    setTimeout('startTime()', 500)
-}
-
-function checkTime(i) {
-    if (i < 10) {
-        i = "0" + i
+String.prototype.format = function(args) {// 格式化字符串
+    var result = this;
+    if (arguments.length > 0) {
+        if (arguments.length == 1 && typeof (args) == "object") {
+            for (var key in args) {
+                if(args[key]!=undefined){
+                    var reg = new RegExp("({" + key + "})", "g");
+                    result = result.replace(reg, args[key]);
+                }
+            }
+        }
+        else {
+            for (var i = 0; i < arguments.length; i++) {
+                if (arguments[i] != undefined) {
+                    var reg= new RegExp("({)" + i + "(})", "g");
+                    result = result.replace(reg, arguments[i]);
+                }
+            }
+        }
     }
-    return i
+    return result;
 }
-var load_nav = function(){
-    var createNav = function(){
-        var adiv = document.createElement('div');
-        adiv.setAttribute('text-align', 'center');
-        adiv.innerHTML =    "<a href=\"..\\index.html\" style=\"font-size:18px;\">主页</a> | "+
-                            "<a href=\"..\\static\\login.html\" style=\"font-size:18px;\">登录</a> | " +
-                            "<a href=\"..\\static\\signup.html\" style=\"font-size:18px;\">注册</a> | " +
-                            "<a href=\"..\\static\\query.html\" style=\"font-size:18px;\">查询</a> | " +
-                            "<a href=\"..\\static\\model.html\" style=\"font-size:18px;\">模板</a>";
-        return adiv
-    };
-    var theNav = document.getElementById("theNav");
-    theNav.setAttribute('text-align', 'center');
-    theNav.appendChild(createNav());
-};
