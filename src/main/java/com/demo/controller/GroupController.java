e89e1d4dacdea9b62bc4bd319b761d8d551f3e64
46436 <464365439@qq.com>
2018-12-13 22:48:33 +0800

    修复了泄露用户隐私的问题

--- src/main/java/com/demo/controller/GroupController.java
+++ src/main/java/com/demo/controller/GroupController.java
@@ -82,7 +82,7 @@ public class GroupController extends Controller {
         Record group_info;
         int user_num;
         try{
-            group_info = Db.findFirst("select * from thegroup,university,object,users where " +
+            group_info = Db.findFirst("select thegroup.*,university.uni_name,users.u_nickname,users.u_photo,object.obj_name from thegroup,university,object,users where " +
                     "thegroup.gro_id="+gro_id+" and thegroup.gro_uni_id = university.uni_id and thegroup.gro_obj_id = object.obj_id and " +
                     "thegroup.gro_manager_id = users.u_id");
             user_num = Db.queryInt("select count(*) from user_group_relation where ugr_gro_id="+gro_id);
